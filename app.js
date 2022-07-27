743b99556544d6faded24b020f81aee21382aed4
emeath <matheus.martins.jeronimo@gmail.com>
2022-07-27 16:14:09 -0300

    Adds books.json and uploads broken app.js

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
 let http = require('http');
+let url = require('url');
+let fs = require('fs');
 
-http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hello World!');
+function readJsonFile() {
+    return new Promise(resolve => {
+        fs.readFile('books.json', 'utf8', (err, data) => {
+            if (err) {
+                console.log(err)
+            }
+            else {
+                return resolve(data);
+            }
+        })
+    })
+}
+
+async function ler() {
+    let dados = await readJsonFile();
+    return dados;
+}
+
+function myRouter(route) {
+    if (route == "/api/books") {
+        let dadosLidos = ler();
+        return dadosLidos.toString();
+    } else {
+        return ":)";
+    }
+}
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    const q = url.parse(req.url, true).query;
+
+    res.write(myRouter(req.url))
+
+    res.write('\n\nHello World!');
+
     res.end();
-    console.log('abc')
 }).listen(1234);
